d38791d65cdf2d81f9e874c8eca0609ab9cd7f6f
Jady Lima <jadylsilva@gmail.com>
2024-07-21 16:27:17 -0300

    Implementação da camada de dominio

---
+++ santander-dev-week-2023/src/main/java/me/dio/santander_dev_week_2023/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package me.dio.santander_dev_week_2023.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.santander_dev_week_2023.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
